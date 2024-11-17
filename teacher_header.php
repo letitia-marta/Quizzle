df890f103a0f790315dee85fd8b71c0ae6979406
Letiția-Marta Iliescu <133118806+letitia-marta@users.noreply.github.com>
2024-11-17 20:52:29 +0200

    Update teacher_header.php

--- teacher_header.php
+++ teacher_header.php
@@ -1,59 +1,31 @@
-<?php
-    include 'config.php';
-
-    if (!isset($_SESSION['teacher_id'])) {
-        header('location:login.php');
-        exit();
-    }
-
-    $teacher_id = $_SESSION['teacher_id'];
-
-    $query = "SELECT c.class_name 
-            FROM classes c
-            WHERE c.teacher = ?";
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param("i", $teacher_id);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    $classes = [];
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $classes[] = $row;
-        }
-    }
-
-    $stmt->close();
-?>
-
 <header class="header">
     <div class="flex">
         <a id="hamburger" class="hamburger">
             <img src="images/menu_icon.png" alt="Buton cu imagine" class="navbar-button-image">
         </a>
 
+        <div class="header-title">
+            <h1>Quizzle <span class="teacher-label">Teacher</span></h1>
+        </div>
+
+
         <nav class="navbar">
             <a href="teacher_page.php">Home</a>
             <a href="admin_contacts.php">Settings</a>
-            <h3>My Classes</h3>
-            <?php 
-            if (!empty($classes)) {
-                foreach ($classes as $class) {
-                    echo '<a href="#">' . htmlspecialchars($class['class_name']) . '</a>';
-                }
-            } else {
-                echo '<p>No Classes Found</p>';
-            }
-            ?>
         </nav>
 
         <div class="icons">
-            <div id="menu-btn" class="fas fa-bars"></div>
-            <div id="user-btn" class="fas fa-user"></div>
+            <a id="menu-btn" class="menu-icon">
+                <img src="images/menu_icon.png" alt="Menu Button" class="icon-button-image">
+            </a>
+            <a id="user-btn" class="user-icon">
+                <img src="images/user_icon.png" alt="User Button" class="icon-button-image">
+            </a>
         </div>
 
+
         <div class="account-box">
-            <p>Name: <span><?php echo $_SESSION['teacher_name']; ?></span></p>
+            <p>Teacher: <span><?php echo $_SESSION['teacher_name']; ?></span></p>
             <p>Email: <span><?php echo $_SESSION['teacher_email']; ?></span></p>
             <a href="logout.php" class="delete-btn">Logout</a>
         </div>
