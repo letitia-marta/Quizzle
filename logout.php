2d4033aa14019f749447e5c7c9eb9e46adac7502
Letiția-Marta Iliescu <133118806+letitia-marta@users.noreply.github.com>
2025-04-12 16:14:21 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
-include 'config.php';
+    include 'config.php';
 
-session_start();
-session_unset();
-session_destroy();
+    session_start();
+    session_unset();
+    session_destroy();
 
-header('location:login.php');
+    header('location:login.php');
 ?>
