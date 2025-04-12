2a5753f097547a8d5a12d2ed3f743399c9c3b031
Letiția-Marta Iliescu <133118806+letitia-marta@users.noreply.github.com>
2025-04-12 16:12:42 +0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,26 @@
+<?php
+    include 'config.php';
+    session_start();
+
+    $user_id = $_SESSION['student_id'];
+
+    if(!isset($user_id))
+    {
+        header('location:login.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>home</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+    
+    </body>
+</html>
